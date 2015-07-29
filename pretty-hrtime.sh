c7c954b7bcc610b096066a82fdc726f19e41e3d0
Eduardo San Martin Morote <i@posva.net>
2015-07-29 09:43:38 +0200

    Added days, hours and minutes. Fixed a bug with 1.xx values

--- pretty-hrtime.sh
+++ pretty-hrtime.sh
@@ -5,16 +5,20 @@
 # This was highly inspired by the node module pretty-hrtime created by robrich
 # at https://github.com/robrich/pretty-hrtime
 
-_desc=(s:1 ms:1000000 μs:1000 ns:1)
+_desc=(d:86400:day h:3600:hour m:60:minute s:1:second ms:1000000:millisecond μs:1000:microsecond ns:1:nanosecond)
 ptime() {
-  local result s ns amount unit convert lconvert precise verbose tmp_str
+  local result s ns amount unit convert lconvert precise verbose tmp_str long
   # TODO check arguments
   # TODO check options
   s="$1"
   ns="$2"
   for unit in ${_desc[@]}; do
     convert=$(echo "$unit" | cut -d: -f2)
-    unit=$(echo "$unit" | cut -d: -f1)
+    if [[ -z "$verbose" ]]; then
+      unit=$(echo "$unit" | cut -d: -f1)
+    else
+      unit=$(echo "$unit" | cut -d: -f3)
+    fi
     # Use the seconds or the nanoseconds amount
     if echo "$unit" | grep '.s$' > /dev/null; then
       amount="$ns"
@@ -22,23 +26,21 @@ ptime() {
       amount="$s"
     fi
     # Remove any extra time already counted
-    # TODO change s by 0-index value
-    if [[ "$unit" != s && "$unit" != ms ]]; then
+    if [[ "$unit" != d && "$unit" != ms ]]; then
       (( amount %= lconvert ))
     fi
     (( val = (100 * amount) /  convert ))
     if [[ "$unit" = s ]]; then
       (( val += ns / 10000000 ))
     fi
-    if [[ "$val" -ge 199 ]]; then
+    if [[ "$val" -ge 100 ]]; then
       # Don't print to many decimals
       if [[ -z "$precise" && "$val" -ge 1000 ]]; then
         (( val /= 100 ))
         tmp_str="$val"
       else
         # We need a tmp var because val is treated as a number
-        tmp_str=$(echo "$val" | sed 's/..$/.&/;t;s/^.$/.0&/')
-        tmp_str=$(echo "$tmp_str" | sed 's/\.0*$//;t;s/00*$//')
+        tmp_str=$(echo "$val" | sed -e 's/..$/.&/' -e 's/^.$/.0&/' -e 's/\.0*$//' -e 's/00*$//')
         if [[ -z "$tmp_str" ]]; then
           tmp_str=0
         fi
@@ -47,7 +49,7 @@ ptime() {
         result="$result "
       fi
       result="${result}${tmp_str} $unit"
-      if [[ -z "$verbose" ]]; then
+      if [[ -z "$long" ]]; then
         break
       fi
     fi
--- tests/test.sh
+++ tests/test.sh
@@ -2,6 +2,31 @@
 
 source pretty-hrtime.sh
 
+testDays() {
+  assertEquals "3 d" "$(ptime 259200)"
+  assertEquals "35 d" "$(ptime 3024000)"
+  assertEquals "35 d" "$(ptime 3024990 904)"
+  assertEquals "1.45 d" "$(ptime 126000)"
+  assertEquals "5.35 d" "$(ptime 462240 6209)"
+  assertEquals "5.1 d" "$(ptime 440640 24332)"
+}
+
+testHours() {
+  assertEquals "3 h" "$(ptime 10800)"
+  assertEquals "15 h" "$(ptime 54000)"
+  assertEquals "15 h" "$(ptime 54000 904)"
+  assertEquals "5.35 h" "$(ptime 19260 6209)"
+  assertEquals "5.1 h" "$(ptime 18360 24332)"
+}
+
+testMinutes() {
+  assertEquals "3 m" "$(ptime 180)"
+  assertEquals "35 m" "$(ptime 2100)"
+  assertEquals "35 m" "$(ptime 2102 904)"
+  assertEquals "5.35 m" "$(ptime 321 6209)"
+  assertEquals "5.1 m" "$(ptime 306 24332)"
+}
+
 testSeconds() {
   assertEquals "3 s" "$(ptime 3)"
   assertEquals "0 s" "$(ptime 0)"
@@ -27,6 +52,14 @@ testMicroSeconds() {
 testNanoSeconds() {
   assertEquals "3 ns" "$(ptime 0 3)"
   assertEquals "301 ns" "$(ptime 0 301)"
+}
+
+testVerbose() {
+  echo TODO
+}
+
+testLong() {
+  echo TODO
 }
 
 SHUNIT_PARENT="$0"
